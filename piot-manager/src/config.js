9846372d88be305b2e807ccbd066edb3a378cba8
michaln <michal.nezerka@gmail.com>
2020-01-07 22:07:47 +0100

    [mgr] Fix detection of API endpoint

--- piot-manager/src/config.js
+++ piot-manager/src/config.js
@@ -1,7 +1,7 @@
 var uri = '/api/'
-// if process package exists (running in node js)
-if (typeof process === 'object') {
-    uri = process.env.PIOT_API ? process.env.PIOT_API  : 'http://localhost:9096/';
+
+if (window.location.hostname === 'localhost') {
+    uri = 'http://localhost:9096/';
 }
 
 export const API = uri
