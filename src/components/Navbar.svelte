65c7974e524cd8f91a02d13a03d5a325801c5fd2
mnezerka <michal.nezerka@sap.com>
2020-02-20 16:14:17 +0100

    Fixed issues with expnding of burger button

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,47 +3,66 @@
     import {link} from 'svelte-spa-router'
     import {profile} from '../stores'
 
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach( el => {
+                el.addEventListener('click', () => {
+
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+    });
+
 </script>
 
 <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
 
-    <div class="container">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/" use:link>
+        PIOT Manager
+        </a>
 
-        <div class="navbar-brand">
-            <a class="navbar-item" href="/" use:link>
-            PIOT Manager
-            </a>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMainMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
 
-            <a href="/" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-
+    <div id="navbarMainMenu" class="navbar-menu">
+        <div class="navbar-start">
+            {#if $authenticated}
+            <a class="navbar-item" href="/orgs" use:link>Organizations</a>
+            <a class="navbar-item" href="/users" use:link>Users</a>
+            <a class="navbar-item" href="/things" use:link>Things</a>
+            {/if}
         </div>
 
-        <div id="navbarMenu" class="navbar-menu">
-
-            <div class="navbar-start">
-                {#if $authenticated}
-                <a class="navbar-item" href="/orgs" use:link>Organizations</a>
-                <a class="navbar-item" href="/users" use:link>Users</a>
-                <a class="navbar-item" href="/things" use:link>Things</a>
-                {/if}
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        {#if $authenticated}
-                        <a href="/signout" use:link class="button is-light">Sign out {#if $profile}{$profile.email}{/if}</a>
-                        {:else}
-                        <a href="/login" use:link class="button is-light">Log in</a>
-                        {/if}
-                    </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    {#if $authenticated}
+                    <a href="/signout" use:link class="button is-light">Sign out {#if $profile}{$profile.email}{/if}</a>
+                    {:else}
+                    <a href="/login" use:link class="button is-light">Log in</a>
+                    {/if}
                 </div>
             </div>
-
         </div>
 
     </div>
